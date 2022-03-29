a4f10e2e834ef88dab81a530b14c7055b5dabe74
Robert Gessler <ragessler@roberts-mbp.lan>
2022-03-29 13:51:27 -0500

    added resources file

---
+++ resources.py
@@ -0,0 +1,17 @@
+import functions
+#global resources
+destinations = ['Washington, D.C', 'New York City, NY', 'Honolulu, HI', 'San Antonio, TX']
+transportation_list = ['Plane', 'Bus', 'Car', 'Train']
+overseas_transportation = ['Plane','Ferry', 'Cruise']
+#Washington DC Resources
+washington_restaurants = ['The Founding Fathers', 'Unconventional Diner','Kinship', 'The Capital Grille', 'Happy Gyro']
+washington_entertainment = ['The Capital Building', 'The Washington Monument', 'The International Spy Musiuem', 'The Arlington National Cemetery', 'The Smithsonian Musems']
+#NYC Resources
+nyc_restaurants = ['Fraunces Tavern', 'Momofuku Ko', 'Per Se', 'Keens Steakhouse', 'Sushi Nakazawa']
+nyc_entertainment = ['the Top of the Empire State Building', 'the Top of The Rock Obeservation Center', 'The American Museum of Natural History', 'St. Patricks Cathedral']
+#Honolulu HI Resources
+honolulu_restaurants = ['Paradise Cove Luau', 'Stripsteak', 'La Mer', 'Orchids', 'Chef Chai', 'By The Sea Restaurant']
+honolulu_entertainment = ['a Dolphin Watch Boat Tour', 'a Submarine Scooter Adventure', 'a Catamaran Cruise', 'The Pearl Harbor Memorial', 'a Helicopter Tour', 'a Shark Dive']
+#San Antonio Resources
+san_antonio_restaurants = ['Kona Grill', 'The Boiler House', "Ruth's Chirs Steakhouse", "Morton's Steakhouse", 'Charma Gaucha', 'J-Prime Steakhouse', 'Up Scale']
+san_antonio_entertainment = ['The Alamo', 'The Ghost City Tour', 'The Natural Bridge Caverns', 'The Witte Museum', 'Six Flags Fiesta Texas', 'The Alamo Beer Factory']
